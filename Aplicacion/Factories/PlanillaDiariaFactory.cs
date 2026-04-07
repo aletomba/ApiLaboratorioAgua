7e41eaa0a9c965d624d893dd33ba70f24690ed8c
Alejandro Tomba <82008062+aletomba@users.noreply.github.com>
2026-04-07 20:03:43 -0300

    feat: extract PlanillaDiariaFactory (#66)
    * feat: extract PlanillaDiariaFactory from PlanillaDiariaService #63
    
    * fix: review - use dto.UnidadMedida, add back-ref in CreateLibroEntrada

---
+++ Aplicacion/Factories/PlanillaDiariaFactory.cs
@@ -0,0 +1,125 @@
+using Dominio.Entities;
+using Infrastructure.Dtos;
+
+namespace Aplicacion.Factories
+{
+    public static class PlanillaDiariaFactory
+    {
+        public static PuntoMuestreo ParsePuntoMuestreo(PuntoMuestreoDto dto) => dto switch
+        {
+            PuntoMuestreoDto.AguaNatural => PuntoMuestreo.AguaNatural,
+            PuntoMuestreoDto.Decantada   => PuntoMuestreo.Decantada,
+            PuntoMuestreoDto.Filtrada    => PuntoMuestreo.Filtrada,
+            PuntoMuestreoDto.Consumo     => PuntoMuestreo.Consumo,
+            _ => throw new ArgumentException("PuntoMuestreo no válido.")
+        };
+
+        public static FisicoQuimico CreateFisicoQuimico(AnalisisPuntoDto analisis, DateTime fecha)
+            => new FisicoQuimico
+            {
+                Fecha = fecha,
+                FechaLLegada = fecha,
+                FechaAnalisis = fecha,
+                Procedencia = analisis.PuntoMuestreo.ToString(),
+                SitioExtraccion = analisis.PuntoMuestreo.ToString(),
+                Ph = analisis.Ph,
+                Turbidez = analisis.Turbidez,
+                Alcalinidad = analisis.Alcalinidad,
+                Dureza = analisis.Dureza,
+                Nitritos = analisis.Nitritos,
+                Cloruros = analisis.Cloruros,
+                Calcio = analisis.Calcio,
+                Magnesio = analisis.Magnesio,
+                Dbo5 = analisis.Dbo5,
+                Cloro = analisis.Cloro,
+            };
+
+        public static Muestra CreateMuestra(AnalisisPuntoDto analisis, PlanillaDiariaDto dto, int clienteId)
+        {
+            var puntoEntity = ParsePuntoMuestreo(analisis.PuntoMuestreo);
+            return new Muestra
+            {
+                Procedencia = analisis.PuntoMuestreo.ToString(),
+                NombreMuestreador = dto.Operador ?? string.Empty,
+                Latitud = 0,
+                Longitud = 0,
+                FechaExtraccion = dto.Fecha,
+                HoraExtraccion = TimeSpan.Zero,
+                TipoMuestra = TipoMuestra.FisicoQuimica,
+                PuntoMuestreo = puntoEntity,
+                ClienteId = clienteId,
+                FisicoQuimico = CreateFisicoQuimico(analisis, dto.Fecha)
+            };
+        }
+
+        public static LibroDeEntrada CreateLibroEntrada(PlanillaDiariaDto dto, List<Muestra> muestras)
+        {
+            var libro = new LibroDeEntrada
+            {
+                Fecha = dto.Fecha,
+                FechaLLegada = dto.Fecha,
+                FechaAnalisis = dto.Fecha,
+                Procedencia = "Planta Potabilizadora",
+                SitioExtraccion = "Planta",
+                Observaciones = dto.Observaciones ?? string.Empty,
+                Muestras = muestras
+            };
+
+            foreach (var muestra in muestras)
+                muestra.LibroEntrada = libro;
+
+            return libro;
+        }
+
+        public static EnsayoJarras CreateEnsayoJarras(EnsayoJarrasDto dto)
+            => new EnsayoJarras
+            {
+                Dosis1 = dto.Dosis1,
+                Dosis2 = dto.Dosis2,
+                Dosis3 = dto.Dosis3,
+                Dosis4 = dto.Dosis4,
+                Dosis5 = dto.Dosis5,
+                DosisSeleccionada = dto.DosisSeleccionada,
+                PreCal = dto.PreCal,
+                PostCal = dto.PostCal,
+                UnidadMedida = dto.UnidadMedida
+            };
+
+        public static PlanillaDiaria CreatePlanilla(PlanillaDiariaDto dto, int libroEntradaId, EnsayoJarras? ensayo)
+            => new PlanillaDiaria
+            {
+                Fecha = dto.Fecha.Date,
+                Operador = dto.Operador,
+                Observaciones = dto.Observaciones,
+                LibroEntradaId = libroEntradaId,
+                EnsayoJarras = ensayo
+            };
+
+        public static void UpdateFisicoQuimico(FisicoQuimico fq, AnalisisPuntoDto analisis)
+        {
+            fq.Ph = analisis.Ph;
+            fq.Turbidez = analisis.Turbidez;
+            fq.Alcalinidad = analisis.Alcalinidad;
+            fq.Dureza = analisis.Dureza;
+            fq.Nitritos = analisis.Nitritos;
+            fq.Cloruros = analisis.Cloruros;
+            fq.Calcio = analisis.Calcio;
+            fq.Magnesio = analisis.Magnesio;
+            fq.Dbo5 = analisis.Dbo5;
+            fq.Cloro = analisis.Cloro;
+        }
+
+        public static void UpdateEnsayoJarras(EnsayoJarras ensayo, EnsayoJarrasDto dto)
+        {
+            ensayo.Dosis1 = dto.Dosis1;
+            ensayo.Dosis2 = dto.Dosis2;
+            ensayo.Dosis3 = dto.Dosis3;
+            ensayo.Dosis4 = dto.Dosis4;
+            ensayo.Dosis5 = dto.Dosis5;
+            ensayo.DosisSeleccionada = dto.DosisSeleccionada;
+            ensayo.PreCal = dto.PreCal;
+            ensayo.PostCal = dto.PostCal;
+            ensayo.UnidadMedida = dto.UnidadMedida;
+        }
+    }
+}
--- Aplicacion/Services/PlanillaDiariaService.cs
+++ Aplicacion/Services/PlanillaDiariaService.cs
@@ -1,3 +1,4 @@
+using Aplicacion.Factories;
 using Aplicacion.Mappers;
 using Infrastructure.Dtos;
 using Dominio.Exceptions;
@@ -80,83 +81,21 @@ namespace Aplicacion.Services
             }
 
             // Crear las muestras con sus análisis fisicoquímicos
-            var muestras = new List<Muestra>();
-            foreach (var analisis in dto.AnalisisPorPunto)
-            {
-                var puntoEntity = MapPuntoMuestreo(analisis.PuntoMuestreo);
-                var muestra = new Muestra
-                {
-                    Procedencia = analisis.PuntoMuestreo.ToString(),
-                    NombreMuestreador = dto.Operador ?? string.Empty,
-                    Latitud = 0,
-                    Longitud = 0,
-                    FechaExtraccion = dto.Fecha,
-                    HoraExtraccion = TimeSpan.Zero,
-                    TipoMuestra = TipoMuestra.FisicoQuimica,
-                    PuntoMuestreo = puntoEntity,
-                    ClienteId = clienteId,
-                    FisicoQuimico = new FisicoQuimico
-                    {
-                        Fecha = dto.Fecha,
-                        FechaLLegada = dto.Fecha,
-                        FechaAnalisis = dto.Fecha,
-                        Procedencia = analisis.PuntoMuestreo.ToString(),
-                        SitioExtraccion = analisis.PuntoMuestreo.ToString(),
-                        Ph = analisis.Ph,
-                        Turbidez = analisis.Turbidez,
-                        Alcalinidad = analisis.Alcalinidad,
-                        Dureza = analisis.Dureza,
-                        Nitritos = analisis.Nitritos,
-                        Cloruros = analisis.Cloruros,
-                        Calcio = analisis.Calcio,
-                        Magnesio = analisis.Magnesio,
-                        Dbo5 = analisis.Dbo5,
-                        Cloro = analisis.Cloro,
-                    }
-                };
-                muestras.Add(muestra);
-            }
+            var muestras = dto.AnalisisPorPunto
+                .Select(a => PlanillaDiariaFactory.CreateMuestra(a, dto, clienteId))
+                .ToList();
 
             // Crear LibroDeEntrada con todas las muestras
-            var libro = new LibroDeEntrada
-            {
-                Fecha = dto.Fecha,
-                FechaLLegada = dto.Fecha,
-                FechaAnalisis = dto.Fecha,
-                Procedencia = "Planta Potabilizadora",
-                SitioExtraccion = "Planta",
-                Observaciones = dto.Observaciones ?? string.Empty,
-                Muestras = muestras
-            };
+            var libro = PlanillaDiariaFactory.CreateLibroEntrada(dto, muestras);
             await _libroEntradaRepo.AddAsync(libro);
 
             // Armar EnsayoJarras si viene en el DTO
-            EnsayoJarras? ensayo = null;
-            if (dto.EnsayoJarras != null)
-            {
-                ensayo = new EnsayoJarras
-                {
-                    Dosis1 = dto.EnsayoJarras.Dosis1,
-                    Dosis2 = dto.EnsayoJarras.Dosis2,
-                    Dosis3 = dto.EnsayoJarras.Dosis3,
-                    Dosis4 = dto.EnsayoJarras.Dosis4,
-                    Dosis5 = dto.EnsayoJarras.Dosis5,
-                    DosisSeleccionada = dto.EnsayoJarras.DosisSeleccionada,
-                    PreCal = dto.EnsayoJarras.PreCal,
-                    PostCal = dto.EnsayoJarras.PostCal,
-                    UnidadMedida = "mg/L"
-                };
-            }
+            var ensayo = dto.EnsayoJarras != null
+                ? PlanillaDiariaFactory.CreateEnsayoJarras(dto.EnsayoJarras)
+                : null;
 
             // Crear la planilla
-            var planilla = new PlanillaDiaria
-            {
-                Fecha = dto.Fecha.Date,
-                Operador = dto.Operador,
-                Observaciones = dto.Observaciones,
-                LibroEntradaId = libro.Id,
-                EnsayoJarras = ensayo
-            };
+            var planilla = PlanillaDiariaFactory.CreatePlanilla(dto, libro.Id, ensayo);
 
             var creada = await _planillaRepo.AddAsync(planilla);
             return (await _planillaRepo.GetByIdAsync(creada.Id)
@@ -174,29 +113,11 @@ namespace Aplicacion.Services
             // Actualizar ensayo de jarras
             if (dto.EnsayoJarras != null && planilla.EnsayoJarras != null)
             {
-                planilla.EnsayoJarras.Dosis1 = dto.EnsayoJarras.Dosis1;
-                planilla.EnsayoJarras.Dosis2 = dto.EnsayoJarras.Dosis2;
-                planilla.EnsayoJarras.Dosis3 = dto.EnsayoJarras.Dosis3;
-                planilla.EnsayoJarras.Dosis4 = dto.EnsayoJarras.Dosis4;
-                planilla.EnsayoJarras.Dosis5 = dto.EnsayoJarras.Dosis5;
-                planilla.EnsayoJarras.DosisSeleccionada = dto.EnsayoJarras.DosisSeleccionada;
-                planilla.EnsayoJarras.PreCal = dto.EnsayoJarras.PreCal;
-                planilla.EnsayoJarras.PostCal = dto.EnsayoJarras.PostCal;
+                PlanillaDiariaFactory.UpdateEnsayoJarras(planilla.EnsayoJarras, dto.EnsayoJarras);
             }
             else if (dto.EnsayoJarras != null && planilla.EnsayoJarras == null)
             {
-                planilla.EnsayoJarras = new EnsayoJarras
-                {
-                    Dosis1 = dto.EnsayoJarras.Dosis1,
-                    Dosis2 = dto.EnsayoJarras.Dosis2,
-                    Dosis3 = dto.EnsayoJarras.Dosis3,
-                    Dosis4 = dto.EnsayoJarras.Dosis4,
-                    Dosis5 = dto.EnsayoJarras.Dosis5,
-                    DosisSeleccionada = dto.EnsayoJarras.DosisSeleccionada,
-                    PreCal = dto.EnsayoJarras.PreCal,
-                    PostCal = dto.EnsayoJarras.PostCal,
-                    UnidadMedida = "mg/L"
-                };
+                planilla.EnsayoJarras = PlanillaDiariaFactory.CreateEnsayoJarras(dto.EnsayoJarras);
             }
 
             // Actualizar análisis físicoquímico por punto
@@ -204,22 +125,13 @@ namespace Aplicacion.Services
             {
                 foreach (var analisis in dto.AnalisisPorPunto)
                 {
-                    var puntoEntity = MapPuntoMuestreo(analisis.PuntoMuestreo);
+                    var puntoEntity = PlanillaDiariaFactory.ParsePuntoMuestreo(analisis.PuntoMuestreo);
                     var muestra = planilla.LibroEntrada.Muestras
                         .FirstOrDefault(m => m.PuntoMuestreo == puntoEntity);
 
                     if (muestra?.FisicoQuimico != null)
                     {
-                        muestra.FisicoQuimico.Ph = analisis.Ph;
-                        muestra.FisicoQuimico.Turbidez = analisis.Turbidez;
-                        muestra.FisicoQuimico.Alcalinidad = analisis.Alcalinidad;
-                        muestra.FisicoQuimico.Dureza = analisis.Dureza;
-                        muestra.FisicoQuimico.Nitritos = analisis.Nitritos;
-                        muestra.FisicoQuimico.Cloruros = analisis.Cloruros;
-                        muestra.FisicoQuimico.Calcio = analisis.Calcio;
-                        muestra.FisicoQuimico.Magnesio = analisis.Magnesio;
-                        muestra.FisicoQuimico.Dbo5 = analisis.Dbo5;
-                        muestra.FisicoQuimico.Cloro = analisis.Cloro;
+                        PlanillaDiariaFactory.UpdateFisicoQuimico(muestra.FisicoQuimico, analisis);
                     }
                 }
             }
@@ -234,15 +146,5 @@ namespace Aplicacion.Services
             await _planillaRepo.DeleteAsync(id);
         }
 
-        // ─── Helpers ────────────────────────────────────────────────────────────
-
-        private static PuntoMuestreo MapPuntoMuestreo(PuntoMuestreoDto dto) => dto switch
-        {
-            PuntoMuestreoDto.AguaNatural => PuntoMuestreo.AguaNatural,
-            PuntoMuestreoDto.Decantada   => PuntoMuestreo.Decantada,
-            PuntoMuestreoDto.Filtrada    => PuntoMuestreo.Filtrada,
-            PuntoMuestreoDto.Consumo     => PuntoMuestreo.Consumo,
-            _ => throw new ArgumentException("PuntoMuestreo no válido.")
-        };
     }
 }
