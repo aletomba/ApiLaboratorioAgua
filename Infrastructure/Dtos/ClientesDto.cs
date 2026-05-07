9302beb9d0b6157d75e9295e96eb1f873ec0016b
Alejandro Tomba <82008062+aletomba@users.noreply.github.com>
2026-05-07 15:50:46 -0300

    feat: agregar Data Annotations de validacion en todos los DTOs (#133)

--- Infrastructure/Dtos/ClientesDto.cs
+++ Infrastructure/Dtos/ClientesDto.cs
@@ -1,10 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Infrastructure.Dtos
 {
     public class ClientesDto
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(200, MinimumLength = 2)]
         public string? Nombre { get; set; }
+
+        [EmailAddress]
+        [StringLength(200)]
         public string? Email { get; set; }
+
+        [Phone]
+        [StringLength(50)]
         public string? Telefono { get; set; }
         //public List<MuestraDto>? Muestras { get; set; }
     }
--- Infrastructure/Dtos/FisicoQuimicoDto.cs
+++ Infrastructure/Dtos/FisicoQuimicoDto.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Infrastructure.Dtos
 {
     public class FisicoQuimicoDto
@@ -6,20 +8,51 @@ namespace Infrastructure.Dtos
         public DateTime Fecha { get; set; }
         public DateTime FechaLLegada { get; set; }
         public DateTime FechaAnalisis { get; set; }
+
+        [Required]
+        [StringLength(500, MinimumLength = 1)]
         public string? Procedencia { get; set; }
+
+        [StringLength(50)]
         public string? Ph { get; set; }
+
+        [StringLength(50)]
         public string? Turbidez { get; set; }
+
+        [StringLength(50)]
         public string? Alcalinidad { get; set; }
+
+        [StringLength(50)]
         public string? Dureza { get; set; }
+
+        [StringLength(50)]
         public string? Nitritos { get; set; }
+
+        [StringLength(50)]
         public string? Nitratos { get; set; }
+
+        [StringLength(50)]
         public string? Amonio { get; set; }
+
+        [StringLength(50)]
         public string? Cloruros { get; set; }
+
+        [StringLength(50)]
         public string? Calcio { get; set; }
+
+        [StringLength(50)]
         public string? Magnesio { get; set; }
+
+        [StringLength(50)]
         public string? Dbo5 { get; set; }
+
+        [StringLength(50)]
         public string? Cloro { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "MuestraId debe ser mayor que 0.")]
         public int MuestraId { get; set; }
+
+        [StringLength(500)]
         public string? MuestraProcedencia { get; set; }
     }
 }
